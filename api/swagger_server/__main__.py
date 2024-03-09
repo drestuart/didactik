60d703c7cfb4c404639592fc20ba21a738951d39
Dan Stuart <1413223+drestuart@users.noreply.github.com>
2024-03-09 12:54:36 -0500

    Enabled CORS on api

--- api/swagger_server/__main__.py
+++ api/swagger_server/__main__.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
 import connexion
-
+from flask_cors import CORS
 from swagger_server import encoder
 
 def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Didactik Mentor API'}, pythonic_params=True)
+    CORS(app.app)
     app.run(port=5000, debug=True)
 
 
